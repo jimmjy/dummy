b38eceada7ab49e7656bdbe269fa14d2ba112650
James Finkelstein <jfinkelstein9@me.com>
2022-02-13 17:48:54 -0500

    small test change

--- 12-http.js
+++ 12-http.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   } else if (req.url === '/about') {
     res.end('Here is our short history');
   } else {
-    res.end(`<h1>Oops!</h1><p>We can't seem to find the page you are looking for</p><a href="/">Back home</a>`);
+    res.end(`<h1>Oops!</h1><p>We can't seem to find the page you are looking fors</p><a href="/">Back home</a>`);
   }
 });
 
